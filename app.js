f37f1b5152d853126b7d168d48ae9b965754e159
manu canolla <73749704+emanuelecanolla@users.noreply.github.com>
2023-11-28 20:29:53 -0300

    adicionando funcionalidade

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
- 
+ const form = document.querySelector("#form")
+ const input = document.querySelector("#search-cities")
+ const msg = document.querySelector(".msg")
+ const list = document.querySelector(".cities")
+
+ const apiKey = '76382fea85d82017486d5daf760d804d'
+
+ form.addEventListener("submit", e =>{
+    e.preventDefault()
+
+    msg.textContent=''
+    msg.classList.remove("visible")
+
+    let inputVal= input.value 
+
+    const ListItemsArray= Array.from(list.querySelectorAll('.cities li'))
+
+    if (ListItemsArray.length > 0) {
+        const filteredArray = ListItemsArray.filter(el => {
+            let content = ''
+            let cityName = el.querySelector('.city-name').textContent.toLowerCase()
+            let cityCountry = el.querySelector('.country').textContent.toLowerCase()
+
+            if(inputVal.includes(',')) {
+                if(inputVal.split(',')[1].length > 2){
+                    inputVal = input.split(',')[0]
+
+                    content = cityName
+                } else{
+                    content = `${cityName},${cityCountry}`
+                }
+            } else {
+                content = cityName
+            }
+
+            return content== inputVal.toLowerCase()
+        })
+
+        console.log(filteredArray)
+    }
+ })
